f0c566897f6c3764d10f2546e0dd1b607845c8ea
HMZ69 <hmuminovic2@etf.unsa.ba>
2018-12-04 18:32:35 +0100

    Sa tutorijala

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,10 +1,48 @@
 package ba.unsa.rpr.tutorijal7;
 
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tutorijal implements Serializable {
 
+    public static ArrayList<Grad> ucitajGradove() {
+        ArrayList<Grad> rValue = new ArrayList<Grad>();
+        try {
+            Scanner ulaz = new Scanner(new FileReader("mjerenja.txt"));
+            String podaciString = new String();
+            int brojac = 0;
+            int pom = 0;
+            int k = 0;
+            String podaci = "";
+            String[] gradovi = new String[1001];
+            while (ulaz.hasNext()) {
+                podaci = ulaz.next();
+                gradovi = podaci.split(",");
+                String naziv = "";
+                double[] temperature = new double[1000];
+                for (int i = 0; i < gradovi.length; i++) {
+                    if (i == 0)
+                        naziv = gradovi[0];
+                    else
+                        temperature[i-1] = Double.parseDouble(gradovi[i]);
+                }
+                rValue.add(new Grad(naziv, 0, temperature));
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return rValue;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    ArrayList<Grad> proba = new ArrayList<Grad>();
+	    proba = ucitajGradove();
+	    for (Grad g : proba)
+	        System.out.println(g);
     }
 }
